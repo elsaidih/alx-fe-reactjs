f8b9b76d9b24ef2c8a1c8b8953d8269931a8091e
el saidi <houcinehoucine943@gmail.com>
2026-01-15 21:07:46 +0000

    new modifications

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,18 @@
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
-
+import WelcomeMessage from './components/WelcomeMessage';
 function App() {
   return (
     <>
+    <div>
+      <WelcomeMessage />
+    </div>
+    <div>
       <Header />
       <MainContent />
       <Footer />
+    </div>
     </>
   );
 }
