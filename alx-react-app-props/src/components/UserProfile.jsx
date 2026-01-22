f65fe62b152a4ab3753cb4ce1bfd81e03c883ab2
el saidi <houcinehoucine943@gmail.com>
2026-01-22 23:22:11 +0000

    did same styling css

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,26 @@
-const UserProfile = (props) => {
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+    <div
+      style={{
+        border: '1px solid gray',
+        padding: '10px',
+        margin: '10px',
+        borderRadius: '8px'
+      }}
+    >
+      <h2 style={{ color: 'blue', marginBottom: '5px' }}>
+        {userData.name}
+      </h2>
+      <p>
+        Email: <span style={{ fontWeight: 'bold' }}>{userData.email}</span>
+      </p>
     </div>
   );
-};
+}
 
 export default UserProfile;
