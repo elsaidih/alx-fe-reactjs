cadcb63c69c79c44805c6dc9c1ea3ea170460f00
el saidi <houcinehoucine943@gmail.com>
2026-01-24 23:29:54 +0000

    did same styling css

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,19 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav
       style={{
-        backgroundColor: 'navy',
-        padding: '15px',
-        display: 'flex',
-        gap: '20px'
+        display: "flex",
+        justifyContent: "space-around",
+        backgroundColor: "navy",
+        padding: "15px"
       }}
     >
-      <Link style={{ color: 'white', textDecoration: 'none' }} to="/">Home</Link>
-      <Link style={{ color: 'white', textDecoration: 'none' }} to="/about">About</Link>
-      <Link style={{ color: 'white', textDecoration: 'none' }} to="/services">Services</Link>
-      <Link style={{ color: 'white', textDecoration: 'none' }} to="/contact">Contact</Link>
+      <Link style={{ color: "white", textDecoration: "none" }} to="/">
+        Home
+      </Link>
+      <Link style={{ color: "white", textDecoration: "none" }} to="/about">
+        About
+      </Link>
+      <Link style={{ color: "white", textDecoration: "none" }} to="/services">
+        Services
+      </Link>
+      <Link style={{ color: "white", textDecoration: "none" }} to="/contact">
+        Contact
+      </Link>
     </nav>
   );
 }
