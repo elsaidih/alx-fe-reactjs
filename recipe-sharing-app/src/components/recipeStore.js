5bf714aef0cfbb3151d3ab15b03e19395fe77c30
el saidi <houcinehoucine943@gmail.com>
2026-02-04 23:23:22 +0000

    did same styling css

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,42 +1,11 @@
 import { create } from "zustand";
 
-const useRecipeStore = create((set, get) => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
-  searchTerm: "",
-  favorites: [],
-  recommendations: [],
-
-  // Add new recipe
-  addRecipe: (newRecipe) =>
-    set((state) => ({
-      recipes: [...state.recipes, newRecipe],
-    })),
-
-  // Search filtering
-  setSearchTerm: (term) => set({ searchTerm: term }),
-  filteredRecipes: () => {
-    const { recipes, searchTerm } = get();
-    return recipes.filter((recipe) =>
-      recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-  },
-
-  // Favorites
-  addFavorite: (recipeId) =>
-    set((state) => ({ favorites: [...state.favorites, recipeId] })),
-  removeFavorite: (recipeId) =>
-    set((state) => ({
-      favorites: state.favorites.filter((id) => id !== recipeId),
-    })),
-
-  // Recommendations (mock example based on favorites)
-  generateRecommendations: () => {
-    const { recipes, favorites } = get();
-    const recommended = recipes.filter(
-      (recipe) => favorites.includes(recipe.id) && Math.random() > 0.5
-    );
-    set({ recommendations: recommended });
-  },
+  addRecipe: (newRecipe) => set((state) => ({
+    recipes: [...state.recipes, newRecipe],
+  })),
+  setRecipes: (recipes) => set({ recipes }),
 }));
 
 export default useRecipeStore;
