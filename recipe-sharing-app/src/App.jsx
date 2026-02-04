befbc474f2f12eafb9b47212ee355491be50b8ac
el saidi <houcinehoucine943@gmail.com>
2026-02-04 22:55:01 +0000

    did same styling css

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,31 @@
+import { Routes, Route } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import SearchBar from "./components/SearchBar";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
   return (
     <div>
       <h1>Recipe Sharing App</h1>
 
-      <SearchBar />
-      <AddRecipeForm />
-      <RecipeList />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <SearchBar />
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+
+        <Route
+          path="/recipes/:id"
+          element={<RecipeDetail />}
+        />
+      </Routes>
     </div>
   );
 }
