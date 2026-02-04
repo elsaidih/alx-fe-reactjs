b6d31b0999b1c88d68f0a9b1334292f4e826df15
el saidi <houcinehoucine943@gmail.com>
2026-02-04 22:04:29 +0000

    did same styling css

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,7 +3,7 @@ import RecipeList from './components/RecipeList';
 
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
+    <div style={{padding: '20px' }}>
       <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
       <RecipeList />
