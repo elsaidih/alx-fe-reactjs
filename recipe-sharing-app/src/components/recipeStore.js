cf506a52baa2153b197b1053363a562a2da1b583
el saidi <houcinehoucine943@gmail.com>
2026-02-04 23:42:04 +0000

    did same styling css

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,15 +2,30 @@ import { create } from "zustand";
 
 const useRecipeStore = create((set, get) => ({
   recipes: [],
-
   searchTerm: "",
-  setSearchTerm: (term) => set({ searchTerm: term }),
 
+  // Add a recipe
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
 
-  setRecipes: (recipes) => set({ recipes }),
+  // Update a recipe by ID
+  updateRecipe: (id, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
 
+  // Delete a recipe by ID
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
+  // Search term actions
+  setSearchTerm: (term) => set({ searchTerm: term }),
+
+  // Filtered recipes based on search term
   filteredRecipes: () => {
     const { recipes, searchTerm } = get();
     return recipes.filter((recipe) =>
