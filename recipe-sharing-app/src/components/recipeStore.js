29da23cccfacb8ba4e861135243b0e39cf0dbe74
el saidi <houcinehoucine943@gmail.com>
2026-02-04 23:29:38 +0000

    did same styling css

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,11 +1,26 @@
 import { create } from "zustand";
 
-const useRecipeStore = create((set) => ({
+const useRecipeStore = create((set, get) => ({
+  // Recipes array
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({
-    recipes: [...state.recipes, newRecipe],
-  })),
+
+  // Search term state
+  searchTerm: "",
+  setSearchTerm: (term) => set({ searchTerm: term }),
+
+  // Actions
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
   setRecipes: (recipes) => set({ recipes }),
+
+  // Computed filtered recipes
+  filteredRecipes: () => {
+    const { recipes, searchTerm } = get();
+    return recipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  },
 }));
 
 export default useRecipeStore;
