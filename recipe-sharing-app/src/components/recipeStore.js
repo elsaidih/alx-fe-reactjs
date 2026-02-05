bb35228883d77c82110e4b56e667d45b35014384
el saidi <houcinehoucine943@gmail.com>
2026-02-05 00:36:22 +0000

    did same styling css

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,18 +1,26 @@
 // src/components/recipeStore.js
 import { create } from "zustand";
 
-const useRecipeStore = create(set => ({
+const useRecipeStore = create((set) => ({
   recipes: [],
-  
-  addRecipe: (recipe) => set(state => ({ recipes: [...state.recipes, recipe] })),
-  
+
+  // Add a new recipe
+  addRecipe: (recipe) =>
+    set((state) => ({ recipes: [...state.recipes, recipe] })),
+
+  // Update an existing recipe by ID
   updateRecipe: (id, updated) =>
-    set(state => ({
-      recipes: state.recipes.map(r => r.id === id ? { ...r, ...updated } : r)
+    set((state) => ({
+      recipes: state.recipes.map((r) =>
+        r.id === id ? { ...r, ...updated } : r
+      ),
     })),
-  
+
+  // Delete a recipe by ID
   deleteRecipe: (id) =>
-    set(state => ({ recipes: state.recipes.filter(r => r.id !== id) }))
+    set((state) => ({
+      recipes: state.recipes.filter((r) => r.id !== id),
+    })),
 }));
 
 export default useRecipeStore;
