cd32294fac33c618988d5ad54375395135c58a6d
el saidi <houcinehoucine943@gmail.com>
2026-02-05 18:59:49 +0000

    did same styling css

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,25 +1,41 @@
-import { Routes, Route } from "react-router-dom";
-import AddRecipeForm from "./components/AddRecipeForm";
-import RecipeList from "./components/RecipeList";
-import RecipeDetail from "./components/RecipeDetail";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetailsWrapper from './components/RecipeDetailsWrapper';
+import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
 
 function App() {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
-        <Route path="/recipes/:id" element={<RecipeDetail />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div style={{ padding: '20px' }}>
+        <header>
+          <h1>Recipe Sharing App</h1>
+          <nav style={{ marginBottom: '20px' }}>
+            <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
+            <Link to="/favorites" style={{ marginRight: '10px' }}>Favorites</Link>
+            <Link to="/recommendations">Recommendations</Link>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchBar />
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
