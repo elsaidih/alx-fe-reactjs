95442044dae0165c0cf8759562aa4413267273bf
el saidi <houcinehoucine943@gmail.com>
2026-02-11 08:43:49 +0000

    Add css tailwind

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,19 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 md:my-20 rounded-lg shadow-lg">
-      <img className="w-32 h-32 md:w-36 md:h-36 rounded-full mx-auto" src="https://via.placeholder.com/150" alt="User" />
-      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center">John Doe</h1>
-      <p className="text-gray-600 sm:w-24 sm:h-24 text-sm md:text-base text-center">Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 md:my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User"
+        className="w-32 h-32 md:w-36 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+      />
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300 ease-in-out">
+        John Doe
+      </h1>
+      <p className="text-gray-600 text-sm md:text-base text-center">
+        Developer at Example Co. Loves to write code and explore new technologies.
+      </p>
     </div>
   );
 }
+
 export default UserProfile;
