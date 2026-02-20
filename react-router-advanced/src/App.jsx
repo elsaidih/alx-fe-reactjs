4fe5517351f7d0cfef54c4d6fabd276de24e3bb1
el saidi <houcinehoucine943@gmail.com>
2026-02-20 23:22:02 +0000

    intial work

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,43 +1,8 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Profile from "./components/Profile";
-import ProfileDetails from "./pages/ProfileDetails";
-import ProfileSettings from "./pages/ProfileSettings";
-import BlogPost from "./pages/BlogPost";
-import Login from "./pages/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <Link to="/">Home</Link> |{" "}
-          <Link to="/profile">Profile</Link> |{" "}
-          <Link to="/blog/1">Blog 1</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          >
-            <Route path="details" element={<ProfileDetails />} />
-            <Route path="settings" element={<ProfileSettings />} />
-          </Route>
-
-          <Route path="/blog/:id" element={<BlogPost />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+<Route
+  path="/profile/*"
+  element={
+    <ProtectedRoute>
+      <Profile />
+    </ProtectedRoute>
+  }
+/>
--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,4 +1,6 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
+import ProfileDetails from "../pages/ProfileDetails";
+import ProfileSettings from "../pages/ProfileSettings";
 
 function Profile() {
   return (
@@ -10,7 +12,11 @@ function Profile() {
         <Link to="settings">Settings</Link>
       </nav>
 
-      <Outlet />
+      {/* Nested Routes Defined Here */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   );
 }
