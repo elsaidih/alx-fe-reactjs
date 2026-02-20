8112eecab47f42a82a2979528cc87d29c17475dc
el saidi <houcinehoucine943@gmail.com>
2026-02-20 23:25:25 +0000

    intial work

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,8 +1,41 @@
-<Route
-  path="/profile/*"
-  element={
-    <ProtectedRoute>
-      <Profile />
-    </ProtectedRoute>
-  }
-/>
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import BlogPost from "./pages/BlogPost";
+import Login from "./pages/Login";
+import Profile from "./components/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/profile">Profile</Link> |{" "}
+          <Link to="/blog/1">Blog 1</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          {/* Protected Route */}
+          <Route
+            path="/profile/*"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Dynamic Route */}
+          <Route path="/blog/:id" element={<BlogPost />} />
+
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
